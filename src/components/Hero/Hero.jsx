9a62f4ecd7ea184318438dccbb0f9ea73643e7f5
Dmytro <az.36419.a@gmail.com>
2024-07-02 11:52:52 +0300

    done hero for all devices

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,3 +1,15 @@
+import ArrowRight from '../../assets/ArrowRight';
+
 export default function Hero() {
-  return <hero></hero>;
+  return (
+    <hero className="block pt-[229px] pl-4 md:pt-[259px] md:pl-16 bg-hero-bg-m md:bg-hero-bg-t lg:bg-hero-bg-d retina:bg-hero-bg-m2x md:retina:bg-hero-bg-t2x lg:retina:bg-hero-bg-d2x min-w-[344px] min-h-[563px] md:min-w-[834px] md:min-h-[600px] lg:min-w-[1440px]bg-no-repeat bg-cover">
+      <h1 className="mb-6 md:mb-8 lg:mb-10 font-extrabold text-5xl text-wht font-inter uppercase max-w-[631px] md:text-[64px] ">
+        будуємо завтра сьогодні
+      </h1>
+      <button className="flex justify-center gap-2.5 items-center w-[198px]  h-14 bg-orn rounded-2xl text-xl text-wht font-bold fontinter ">
+        <span className="h-8">До каталогу</span>
+        <ArrowRight />
+      </button>
+    </hero>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,12 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     screens: {
-      sm: '360px',
+      sm: '344px',
       md: '834px',
       lg: '1440px',
+      retina: {
+        raw: '(min-resolution: 192dpi), (min-resolution: 2dppx), (min-device-pixel-ratio: 2)',
+      },
     },
     container: {
       padding: {
@@ -39,6 +42,17 @@ export default {
       wide: '.025em',
       wider: '.05em',
       widest: '.1em',
+    },
+    backgroundImage: {
+      'hero-bg-m': "url('./public/images/mobile/hero_background_mobile.jpg')",
+      'hero-bg-m2x':
+        "url('./public/images/mobile/hero_background_mobile@2x.jpg')",
+      'hero-bg-t': "url('./public/images/tablet/hero_background_tablet.jpg')",
+      'hero-bg-t2x':
+        "url('./public/images/tablet/hero_background_tablet@2x.jpg')",
+      'hero-bg-d': "url('./public/images/desktop/hero_background_desktop.jpg')",
+      'hero-bg-d2x':
+        "url('./public/images/desktop/hero_background_desktop@2x.jpg')",
     },
     extend: {},
   },
